c89dafdd35ab184998ce8b9e4ac03e7794756bc0
SteimberAZ <randyarteaga1519@gmail.com>
2025-02-20 11:34:06 -0500

    Update javas.js

--- javas.js
+++ javas.js
@@ -6,6 +6,8 @@ var amarillo1 = false;
 var azul1 = false;
 var verde1 = false;
 let aumente = 0; // Inicializamos la variable fuera de la función
+var piel = false;
+var color =  true;
 document.addEventListener("DOMContentLoaded" ,function(){
         let contenedor = document.getElementById("Madre");
         for(let i = 0; i< 256;i++){
@@ -18,6 +20,12 @@ document.addEventListener("DOMContentLoaded" ,function(){
         div.style.backgroundColor= "white";
         div.style.transition = "all 0.5s ease-in-out";
         }
+        var input = document.getElementById("color");
+        var boton2 = document.getElementById("color1");
+        input.addEventListener("input",()=>{
+        boton2.style.backgroundColor = input.value ||"transparent";
+        
+    });
 });
 
 
@@ -37,23 +45,45 @@ function Contador(event) {
     // Obtener las imágenes y restablecerlas al inicio
     let gameu1 = document.getElementById("GAMES1");
     let gameu2 = document.getElementById("GAMES2");
+    let gameu3 = document.getElementById("GAMES3");
+    let gameu4 = document.getElementById("GAMES4");
+    let gameu5 = document.getElementById("GAMES5");
+    
 
     gameu1.style.opacity = "0";
     gameu2.style.opacity = "0";
     gameu1.style.filter = "blur(35px)";
     gameu2.style.filter = "blur(35px)";
+    gameu3.style.opacity = "0";
+    gameu4.style.opacity = "0";
+    gameu3.style.filter = "blur(35px)";
+    gameu4.style.filter = "blur(35px)";
+    gameu5.style.opacity = "0";
+    gameu5.style.filter = "blur(35px)";
 
-    // Mostrar la primera imagen (GAMES1) y aplicar blur si "aumente" es 0
+    
     if (aumente === 0) {
         gameu1.style.opacity = "1";
         gameu1.style.filter = "blur(35px)";
     } else if (aumente === 1) {
-        // Cambiar a la segunda imagen (GAMES2) si "aumente" es 1
+      
         gameu2.style.opacity = "1";
         gameu2.style.filter = "blur(35px)";
+    }else if (aumente === 2) {
+      
+        gameu3.style.opacity = "1";
+        gameu3.style.filter = "blur(35px)";
+    }else if (aumente === 3) {
+      
+        gameu4.style.opacity = "1";
+        gameu4.style.filter = "blur(35px)";
+    }else if (aumente === 4) {
+      
+        gameu5.style.opacity = "1";
+        gameu5.style.filter = "blur(35px)";
     }
 
-    let i = 59; // Comienza el contador en 60
+    let i = 3; // Comienza el contador en 60
     let interval = setInterval(() => {
         let elementos = document.getElementById("aumentador");
         elementos.textContent = i;
@@ -69,9 +99,18 @@ function Contador(event) {
             } else if (aumente === 1) {
                 gameu2.style.filter = "blur(0px)";
             }
+             else if (aumente === 2) {
+                gameu3.style.filter = "blur(0px)";
+            }
+            else if (aumente === 3) {
+                gameu4.style.filter = "blur(0px)";
+            }   
+            else if (aumente === 4) {
+                gameu5.style.filter = "blur(0px)";
+            }  
 
             // Alternar el valor de "aumente" entre 0 y 1 para cambiar de imagen
-            aumente = (aumente + 1) % 2; // Cambiar entre 0 y 1
+            aumente = (aumente + 1) ; // Cambiar entre 0 y 1
         }
     }, 1000); // Se actualiza cada 1 segundo
 }
@@ -124,8 +163,12 @@ function borrar(){
 }
 function Seleccionar() {
 
-    var imagen = event.target
-    
+    var imagen = event.target;
+    var input = document.getElementById("color");
+    var boton2 = document.getElementById("color1");
+    input.addEventListener("input",()=>{
+        boton2.style.backgroundColor = input.value ||"transparent";
+        });
     if(imagen!== 1){
         if (negro1){
             imagen.style.backgroundColor = "black";
@@ -150,10 +193,24 @@ function Seleccionar() {
         else if (verde1){
             imagen.style.backgroundColor = "green";
         }
+        else if (piel){
+            imagen.style.backgroundColor = "bisque";
+        }
+        else if (color){
+            imagen.style.backgroundColor = input.value;
+        }
            
     }
-  
 }
+function seli1(){
+    var salu = event.target;
+    
+    salu.style.transform = "translateY(-1000px)";
+
+    salu.style.transition = "all 1s ease-in-out";
+    salu.style.opacity = 0;
+}
+
 function negro() {
     negro1 = true;
     blanco1 = false;
@@ -161,30 +218,38 @@ function negro() {
     amarillo1 = false;
     azul1 = false;
     verde1 = false;
+    piel = false;
+    color = false;
 }
 function blanco() {
     blanco1 = true;
     negro1 = false;
     rojo1 = false;
     amarillo1 = false;
+    piel = false;
     azul1 = false;
     verde1 = false;
+    color = false;
 }  
 function rojo() {
     rojo1 = true;
     blanco1 = false;
     negro1 = false;
+    piel = false;
     amarillo1 = false;
     azul1 = false;
     verde1 = false;
+    color = false;
 }  
 function amarillo() {
     amarillo1 = true;
     blanco1 = false;
     rojo1 = false;
     negro1 = false;
+    piel = false;
     azul1 = false;
     verde1 = false;
+    color = false;
 }  
 function azul() {
     azul1 = true;
@@ -193,12 +258,37 @@ function azul() {
     amarillo1 = false;
     negro1 = false;
     verde1 = false;
+    color = false;
+    piel = false;
 }  
 function verde() {
     verde1 = true;
     azul1 = false;
     blanco1 = false;
+    piel = false;
     rojo1 = false;
     amarillo1 = false;
     negro1 = false;
+    color = false;
+} 
+function piel1() {
+    verde1 = false;
+    azul1 = false;
+    blanco1 = false;
+    rojo1 = false;
+    amarillo1 = false;
+    piel = true;
+    negro1 = false;
+    color = false;
 }  
+function color1() {
+    verde1 = false;
+    azul1 = false;
+    blanco1 = false;
+    rojo1 = false;
+    amarillo1 = false;
+    piel = false;
+    negro1 = false;
+    color = true;
+}  
+
